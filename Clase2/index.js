5bef449a65c9006e7b059ef128b091b3ccdde01b
Mafe <mafejh274@gmail.com>
2023-03-18 11:20:38 -0500

    Clase 18 marzo

---
+++ Clase2/index.js
@@ -0,0 +1,26 @@
+const http = require("http");
+
+const HOST = "localhost";
+const PORT = 8000;
+
+const writeHTMLResponse = (res, htmlCode) => {
+  res.setHeader("Content-Type", "text/html");
+  res.writeHead(200);
+  res.end(htmlCode);
+  //res.end("Este es mi primer servidor"); //Creo instancia del servidor
+};
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  console.log("URL es: ", url);
+
+  if (url === "/other") {
+    writeHTMLResponse(res, "<p> Esta es sotra ruta </p>");
+  } else {
+    writeHTMLResponse(res, "<p> Codigo HTML </p>");
+  }
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Servidor corriendo en http://${HOST}:${PORT}`);
+});
