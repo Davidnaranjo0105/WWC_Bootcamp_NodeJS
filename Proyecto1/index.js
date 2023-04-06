4ad520d0714109e0a3f1d905a69593888fb3ea5f
Mafe <mafejh274@gmail.com>
2023-04-06 14:38:49 -0500

    Probando el patch

--- Proyecto1/index.js
+++ Proyecto1/index.js
@@ -6,7 +6,7 @@ const app = express();
 const urlPath = "/api/v1/products/";
 app.use(express.json()); 
 
-const products = [
+let products = [
     { id: 1,
       name: "Chunky salmón y cordero",
       description: "Descripcion",
@@ -38,10 +38,13 @@ const products = [
     res.send("Pagina principal"); //Texto plano de respuesta
 });
 
-fs.writeFile('lista-productos.txt', JSON.stringify(products), (err) => {
+
+function crearTxt (listadoActual) {
+  fs.writeFile('lista-productos.txt', JSON.stringify(listadoActual), (err) => {
   if (err) throw err;
   console.log('La lista de productos se ha almacenado en el archivo lista-productos.txt');
 });
+}
 
 app.get(urlPath,(req, res)=>{
     res.json(products); // Devuelve toda la lista de productos
@@ -52,23 +55,23 @@ app.get(urlPath,(req, res)=>{
     const product = {id,name,description,price,available_units,category};
     products.push(product); //creado con su identificador único asignado.                         
     res.json(products);
-});
-fs.writeFile('lista-productos.txt', JSON.stringify(products), { flag: 'a+' }, function (err) {
-  if (err) throw err;
-  console.log('Se escribió en el archivo');
+    crearTxt(products);
 });
 
  app.patch("/api/v1/products/:id",(req,res)=>{ // Modificar un producto y devolverlo
-     const id = parseInt(req.params);
+     const {id} = req.params;
     const productoModificado = req.body;
-    let productoEncontrado = products.find(producto => producto.id === id);
-  
-  console.log(req.params);
+    console.log("req.body ", req.body);
+    console.log("products ", products);
+    const productoEncontrado = products.find(producto => producto.id === id);
+  // console.log("encontrado "+productoEncontrado);
+  // console.log("modificado "+productoModificado);
      if (productoEncontrado) {
-      for (let key in productoModificado) {
-        productoEncontrado[key] = productoModificado[key];
-      }
-      res.json(productoEncontrado);
+      // for (let key in productoModificado) {
+      //   productoEncontrado[key] = productoModificado[key];
+      // }
+      // res.json(productoEncontrado);
+      // crearTxt(products);
     } else {
       res.status(404).send('Producto no encontrado');
     } 
@@ -82,8 +85,8 @@ console.log(indiceProducto);
    if (indiceProducto) {
     products.splice(indiceProducto);
     res.send('Producto eliminado correctamente');
+    crearTxt(products);
    }
-
 });  
 
 app.listen(PORT, () => {
